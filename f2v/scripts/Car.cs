6fe5f4ed9fb56ee10b5a4404abe48153b963ce86
Rimsoo <abdeldebeche@gmail.com>
2024-11-17 21:44:28 +0100

    managed airroll & double jumps

--- f2v/scripts/Car.cs
+++ f2v/scripts/Car.cs
@@ -4,54 +4,86 @@ using System;
 public partial class Car : VehicleBody3D
 {
     [Export] public float Power = 100.0f; // Vitesse de la voiture
-    [Export] public float MaxSteer = 0.9f; // Vitesse de la voiture
+    [Export] public float MaxSteer = 0.9f; // Vitesse de direction maximale
     [Export] public float TurnSpeed = 3.0f; // Vitesse de rotation
-    [Export] public float JumpVelocity = 10.0f;
-    [Export] public float BreakForce = 10.0f;
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+    [Export] public float BreakForce = 10.0f; // Force de freinage
+    [Export] public float JumpForce = 300.0f; // Force de saut
+    [Export] public float AirRotationSpeed = 6.0f; // Vitesse de rotation dans les airs
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		Steering = Mathf.MoveToward(Steering, (Input.GetActionStrength("turn_left") - Input.GetActionStrength("turn_right")) * MaxSteer, (float)delta * 10);
-		EngineForce = (Input.GetActionStrength("move_forward") - Input.GetActionStrength("move_backward")) * Power;
-		Brake = Input.GetActionStrength("brake") * BreakForce;
-		GD.Print(Brake);
+    private VehicleWheel3D frontLeftWheel;
+    private VehicleWheel3D frontRightWheel;
+    private VehicleWheel3D backLeftWheel;
+    private VehicleWheel3D backRightWheel;
 
-        // Gestion du saut
-		// if (Input.IsActionJustPressed("jump") && IsOnFloor())
-        // {
-        //     velocity.Y = JumpVelocity;
-        // }
-		
-        // // Accélération et marche arrière avec `RT` et `LT`
-        // float forwardInput = Input.GetActionStrength("move_forward") - Input.GetActionStrength("move_backward");
-		
-        // // Rotation de la voiture avec le joystick gauche, inversée
-        // float leftRightTurnInput = Input.GetActionStrength("turn_right") - Input.GetActionStrength("turn_left");
-        // float upDownTurnInput = 0;
-        // float rollInput = 0;
-        // if (!IsOnFloor())
-        // {
-        //     upDownTurnInput = Input.GetActionStrength("turn_up") - Input.GetActionStrength("turn_down"); 
-        //     rollInput = Input.GetActionStrength("roll_right") - Input.GetActionStrength("roll_left");
-        // }
+    private bool CanDoubleJump = false;
 
-        // // Rotation de la voiture avec le joystick gauche, inversée
-        // float leftRightTurnInput = Input.GetActionStrength("turn_right") - Input.GetActionStrength("turn_left");
-        // float upDownTurnInput = 0;
-        // float rollInput = 0;
-        // if (!IsOnFloor())
-        // {
-        //     upDownTurnInput = Input.GetActionStrength("turn_up") - Input.GetActionStrength("turn_down"); 
-        //     rollInput = Input.GetActionStrength("roll_right") - Input.GetActionStrength("roll_left");
-        // }
-        // Rotation = new Vector3(Rotation.X - upDownTurnInput * TurnSpeed * (float)delta, Rotation.Y - leftRightTurnInput * TurnSpeed * (float)delta, Rotation.Z - rollInput * TurnSpeed * (float)delta);
-        // // Mise à jour de la vélocité en Y et déplacement
-		// Velocity = velocity;
-        // MoveAndSlide();
-	}
+    public override void _Ready()
+    {
+        // Récupère chaque roue
+        frontLeftWheel = GetNode<VehicleWheel3D>("WheelFrontLeft");
+        frontRightWheel = GetNode<VehicleWheel3D>("WheelFrontRight");
+        backLeftWheel = GetNode<VehicleWheel3D>("WheelBackLeft");
+        backRightWheel = GetNode<VehicleWheel3D>("WheelBackRight");
+    }
+
+    public bool AreAllWheelsTouching()
+    {
+        // Vérifie si toutes les roues touchent une surface
+        return frontLeftWheel.IsInContact() &&
+               frontRightWheel.IsInContact() &&
+               backLeftWheel.IsInContact() &&
+               backRightWheel.IsInContact();
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        // Gestion de la direction et de la force moteur
+        Steering = Mathf.MoveToward(Steering, (Input.GetActionStrength("turn_left") - Input.GetActionStrength("turn_right")) * MaxSteer, (float)delta * 10);
+        EngineForce = (Input.GetActionStrength("move_forward") - Input.GetActionStrength("move_backward")) * Power;
+        Brake = Input.GetActionStrength("brake") * BreakForce;
+
+        // Contrôle dans les airs
+        if (!AreAllWheelsTouching())
+        {
+            AirControl(delta);
+        }
+        else
+        {
+            // Réinitialise le double saut
+            CanDoubleJump = true;
+        }
+
+        CheckJumps();
+    }
+
+    private void AirControl(double delta)
+    {
+        // Entrées utilisateur pour la rotation
+        float pitchInput = Input.GetActionStrength("turn_up") - Input.GetActionStrength("turn_down");
+        float rollInput = Input.GetActionStrength("roll_right") - Input.GetActionStrength("roll_left");
+
+        // Rotation instantanée
+        Vector3 angularVelocity = AngularVelocity;
+
+        // Modifie directement la vélocité angulaire
+        angularVelocity.X = pitchInput * AirRotationSpeed;
+        angularVelocity.Z = rollInput * AirRotationSpeed;
+
+        // Applique la nouvelle vélocité angulaire
+        AngularVelocity = angularVelocity;
+    }
+
+    private void CheckJumps()
+    {
+        // Saut simple
+        if (Input.IsActionJustPressed("jump") && AreAllWheelsTouching())
+        {
+            ApplyImpulse(Vector3.Up * JumpForce);
+        } 
+        else if (Input.IsActionJustPressed("jump") && CanDoubleJump )
+        {
+            ApplyImpulse(Vector3.Up * JumpForce);
+            CanDoubleJump = false;
+        }
+    }
 }
