46322cf1062c7831187c80435dee989d743da061
Rimsoo <abdeldebeche@gmail.com>
2024-11-18 04:01:52 +0100

    Start Implementing ball cam lock

--- f2v/scripts/Player.cs
+++ f2v/scripts/Player.cs
@@ -8,11 +8,14 @@ public partial class Player : CharacterBody3D
     [Export] public float MaxCameraAngle = 80.0f; // Angle maximal de la caméra
     [Export] public float CamHeihgtY = 3.0f; // Hauteur de la caméra
     [Export] public float CamDistanceZ = 10.0f; // Distance de la caméra
-    [Export] public float CamRotationX = 15.0f; // Distance de la caméra
+    [Export] public float CamRotationX = -20.0f; // Distance de la caméra
+    [Export] public bool IsBallCamLock = false; // Verrouillage de la caméra sur la balle
 
     private Vector3 CameraBaseRotation; // Rotation de base de la caméra
     private VehicleBody3D Car;
-    private Node3D TwistPivot;
+    private Node3D CarPivot;
+    private Node3D Ball;
+    private Camera3D Camera;
 
     public override void _Ready()
     {
@@ -20,34 +23,46 @@ public partial class Player : CharacterBody3D
         Car = GetNode<VehicleBody3D>("Car");
 
         // Récupère le pivot de rotation
-        TwistPivot = GetNode<Node3D>("Car/TwistPivot");
+        CarPivot = GetNode<Node3D>("Car/CarPivot");
+        Camera = CarPivot.GetNode<Camera3D>("Camera");
+        Ball = GetTree().GetNodesInGroup("Ball")[0] as Node3D;
     }
 
     public override void _PhysicsProcess(double delta)
     {
+        IsBallCamLock = Input.IsActionJustPressed("camera_lock") ? !IsBallCamLock : IsBallCamLock;
+        // Le twist pivo va suivre parfaitement la voiture
+        CarPivot.Rotation = new Vector3(CarPivot.Rotation.X, Car.Rotation.Y, CarPivot.Rotation.Z);
+        CarPivot.Position = new Vector3(Car.Position.X, Car.Position.Y + CamHeihgtY, Car.Position.Z + CamDistanceZ);
+
+        if(IsBallCamLock)
+        {
+            CarPivot.LookAt(Ball.GlobalTransform.Origin - new Vector3(0, CamHeihgtY, 0));
+            Camera.LookAt(Ball.GlobalTransform.Origin -  CarPivot.GlobalTransform.Origin);
+        }
+        else
+        {
+            
+            Camera.Rotation = new Vector3(Mathf.DegToRad(CamRotationX), 0, 0);
+        }
+
         // Contrôle de la caméra avec le joystick droit
         float cameraHorizontal = Input.GetActionStrength("camera_right") - Input.GetActionStrength("camera_left");
-        // Le twist pivo va suivre parfaitement la voiture
-        TwistPivot.Rotation = new Vector3(TwistPivot.Rotation.X, Car.Rotation.Y, TwistPivot.Rotation.Z);
-        TwistPivot.Position = new Vector3(Car.Position.X, Car.Position.Y + CamHeihgtY, Car.Position.Z + CamDistanceZ);
 
         if(cameraHorizontal != 0) 
         {
             // Rotation de la caméra avec limitation de l'angle
-            float newCameraRotation = TwistPivot.Rotation.Y - cameraHorizontal * CameraSensitivity;
+            float newCameraRotation = CarPivot.Rotation.Y - cameraHorizontal * CameraSensitivity;
             newCameraRotation = Mathf.Clamp(newCameraRotation, -Mathf.DegToRad(MaxCameraAngle), Mathf.DegToRad(MaxCameraAngle));
-            TwistPivot.Rotation = new Vector3(TwistPivot.Rotation.X, newCameraRotation, TwistPivot.Rotation.Z);
-        }
-
-        // Si aucun mouvement n'est détecté, retour progressif de la caméra à sa position de base
-        if (cameraHorizontal == 0)
+            CarPivot.Rotation = new Vector3(CarPivot.Rotation.X, newCameraRotation, CarPivot.Rotation.Z);
+        } 
+        else
         {
-            TwistPivot.Rotation = new Vector3(
-                TwistPivot.Rotation.X,
-                Mathf.Lerp(TwistPivot.Rotation.Y, Car.Rotation.Y, CameraResetSpeed * (float)delta),
-                TwistPivot.Rotation.Z
+            CarPivot.Rotation = new Vector3(
+                CarPivot.Rotation.X,
+                Mathf.Lerp(CarPivot.Rotation.Y, Car.Rotation.Y, CameraResetSpeed * (float)delta),
+                CarPivot.Rotation.Z
             );
         }
-
     }
 }
